f438dad42269548088d5ce858b0e1b3dfcd55340
Samarth Rathore <samarthrathore.business@gmail.com>
2025-06-18 11:13:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const {connectToMongoDB}= require("./connect")
+const urlRoute= require('./routes/url');
+const URL= require("./models/url")
+const app= express();
+const PORT= 8001;
+
+// connectToMongoDB('mongodb://localhost:27017/short-url')
+connectToMongoDB('mongodb+srv://samarthrathore220022:BLzjcCh6lZPRyuwa@cluster0.9idl2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+.then(()=> console.log('Mongodb connected'));
+
+app.use(express.json());
+app.use("/url", urlRoute);
+app.use(express.urlencoded({ extended: true }));
+app.get('/:shortId', async(req, res)=>{
+    const shortId= req.params.shortId;
+    const entry= await URL.findOneAndUpdate({
+        shortId
+    }, {$push: {
+        visitHistory:{
+            timestamp: Date.now(),
+        }
+    },
+});
+res.redirect(entry.redirectURL)
+})
+
+app.listen(PORT, ()=> console.log(`Server Started at PORT:${PORT}`));
